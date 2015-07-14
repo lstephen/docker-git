1db6ec1ebc96ec48f0c5f422834c59b0bd9cf91a
lestephen <lestephen@groupon.com>
2015-07-14 09:53:30 -0700

    Initial commit

---
+++ construi/publish/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+$VERSION_LATEST=$VERSION_2_4
+$VERSOIN_2_4=2.4.5
+
+docker build --tag lstephen/git:$VERSION_2_4 2.4/
+docker tag lstephen/git:$VERSION_2_4 lstephen/git:2.4
+
+docker tag lstephen/git:$VERSION_LATEST lstephen/git:latest
+
+
+docker login --username=$DOCKERHUB_USERNAME --password=$DOCKERHUB_PASSWORD
+docker push lstephen/git:latest
+docker push lstephen/git:2.4
+docker push lstephen/git:$VERSION_2_4
+
